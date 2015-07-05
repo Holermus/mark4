b1860f39e413391e09f3362a720bad814b4f1e3e
Your Name <misakpopi@gmail.com>
2015-07-05 23:45:01 +0200

    S3 parametry nastaveny + zmeny v controlleru a user_page viewsu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def create
+    @user = User.new(post_params)
+
+    if @user.save
+      redirect_to profile_path
+     else
+      redirect_to profile_path
+    end
+  end
+  
   def update
     @user.update(post_params)
   end
-  
   
   private
   
