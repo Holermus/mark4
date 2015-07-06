91eb26da645423e78fc9039a5ebc1bf29cb24452
Your Name <misakpopi@gmail.com>
2015-07-06 16:01:36 +0200

    nastavena path pro create, zmena na POST akci, zkusit 6.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,16 @@ class UsersController < ApplicationController
     @user = User.save
   end
   
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update_attributes( user_params )
+      redirect_to profile_path
+    else
+      redirect_to profile_path
+    end
+  end
+  
   private
   
   def user_params
