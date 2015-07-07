5c758b1031f429cd9449bbbc69d5c58992a6ee9e
Your Name <misakpopi@gmail.com>
2015-07-08 01:19:19 +0200

    precompile 2.0

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module Mark4
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_files = true
+    #precompile css pro produkci
+    config.assets.precompile += [
+      'application.css'
+    ]
   end
 end
