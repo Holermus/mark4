1d0bdec4b41da205ea4d5816685ea66d682faa25
Holermus <misakpopi@gmail.com>
2015-09-12 12:59:50 +0200

    save po uprave assignment user vs post

--- app/models/user_post.rb
+++ app/models/user_post.rb
@@ -1,2 +1,7 @@
 class UserPost < ActiveRecord::Base
+	attr_accessible :content
+	belongs_to :user
+	validates :content, :presence => true, :length => { :maximum => 7000 }
+	validates :user_id, :presence => true
+	default_scope :order => 'user_posts.created_at DESC'
 end
