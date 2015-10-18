dd2bc9d04c185e060e1647aa77f8e23263855975
Holermus <misakpopi@gmail.com>
2015-10-18 21:22:32 +0200

    good looking profile view, before adding functions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912102151) do
+ActiveRecord::Schema.define(version: 20151006181130) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -37,12 +37,12 @@ ActiveRecord::Schema.define(version: 20150912102151) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                   default: "", null: false
-    t.string   "encrypted_password",      default: "", null: false
+    t.string   "email",                   default: "",    null: false
+    t.string   "encrypted_password",      default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",           default: 0,  null: false
+    t.integer  "sign_in_count",           default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150912102151) do
     t.string   "profilepic_content_type"
     t.integer  "profilepic_file_size"
     t.datetime "profilepic_updated_at"
+    t.boolean  "admin",                   default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
